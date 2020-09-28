18e302ba20859b24cc69bf222355dae6e906e444
Jesus Armando Zamora Gamez <armando.zamora14@hotmail.com>
2020-09-27 22:12:14 -0700

    App 04

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import PageTitle from './components/PageTitle';
+
+export default class App extends Component{
+
+  render(){
+    return(
+      <PageTitle text='Taquería Don Erik' color='cyan'/>
+    );
+  }
+}
